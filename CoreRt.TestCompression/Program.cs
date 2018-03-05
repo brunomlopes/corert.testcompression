431243812ef4384f0b935f4b28b12c8a2760798b
Bruno Lopes <brunomlopes@gmail.com>
2018-03-05 00:47:51 +0000

    Publish this with  dotnet publish -c Release -r win-x64 , try to run with CoreRt.TestCompression.exe ..\sample-zip.zip at CoreRt.TestCompression\bin\Release\netcoreapp2.0\win-x64\native and it will fail with "Unhandled Exception: System.IO.Compression.ZLibException: The underlying compression routine could not be loaded correctly. ---> System.DllNotFoundException: Unable to load DLL 'clrcompression.dll': The specified module could not be found."

---
+++ CoreRt.TestCompression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace CoreRt.TestCompresssion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using(var stream = File.OpenRead(args[0]))
+            using (var compressed = new ZipArchive(stream, ZipArchiveMode.Read))
+            {
+                foreach (var zipArchiveEntry in compressed.Entries)
+                {
+                    using (var fileStream = zipArchiveEntry.Open())
+                    using (var outputStream = new MemoryStream())
+                    {
+                        fileStream.CopyTo(outputStream);
+                    }
+
+                    Console.WriteLine($"Wrote {zipArchiveEntry.FullName}");
+                }
+            }
+
+        }
+    }
+}
